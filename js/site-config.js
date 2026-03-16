1c06d05bf9e31a6d8699a592c44a36646f9a0372
Benas Vaičiulis <benasvaiciulis@Benass-MacBook-Air.local>
2026-03-16 19:37:32 +1100

    Add Google Analytics tracking

--- js/site-config.js
+++
@@ -1,5 +0,0 @@
-// Central place for site configuration shared across pages.
-// Set this to your GA4 Measurement ID (format: G-XXXXXXXXXX).
-// You can find it in Google Analytics: Admin -> Data streams -> Measurement ID.
-window.SITE_CONFIG = window.SITE_CONFIG || {};
-window.SITE_CONFIG.gaMeasurementId = window.SITE_CONFIG.gaMeasurementId || "G-S667S7VDV1";
